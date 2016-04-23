efb2eaa19b5e6fb73bf2508e2f6fb3d085449057
Chris Samuelson <chris.sam55@gmail.com>
2016-04-22 20:40:13 -0400

    Updated to test all three frameworks.

--- main.cc
+++ main.cc
@@ -3,25 +3,27 @@
 #include<vector>
 #include<map>
 #include"openglsystem.hh"
+#include"directxsystem.hh"
+#include"vulkansystem.hh"
 
 int main(){
   //GraphicsSystem<int> gsi;
   OpenGLSystem ogs;
-  //DirectXSystem dxs;
-  //VulkanSystem vks;
+  DirectXSystem dxs;
+  VulkanSystem vks;
 
   OpenGLSystem::shader ogshader("foo", "bar");
-  //DirectXSystem::shader dxshader("foo", "bar");
-  //VulkanSystem::shader vkshader("foo", "bar");
+  DirectXSystem::shader dxshader("foo", "bar");
+  VulkanSystem::shader vkshader("foo", "bar");
   
   //gsi.foo();
   ogs.foo();
-  //dxs.foo();
-  //vks.foo();
+  dxs.foo();
+  vks.foo();
   
   ogshader();
-  //dxshader();
-  //vkshader();
+  dxshader();
+  vkshader();
   
   return 0;
 }
