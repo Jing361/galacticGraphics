9c66296005483547bb9e323e40441e5b726b2417
Chris Samuelson <chris.sam55@gmail.com>
2016-05-20 13:57:20 -0400

    Cleaned up enough errors to compile. yay.

--- main.cc
+++ main.cc
@@ -16,15 +16,16 @@ int main(){
   typedef system::meshManager       meshManager;
   typedef system::materialManager   materialManager;
   typedef system::scene             scenemanager;
+  typedef scenenode<system>         node;
 
   OpenGLSystem gfx;
-  scenemanager& scene = gfx.getManager(mainScene);
+  std::string mainScene = "main";
+  scenemanager& scene = gfx.getSceneManager(mainScene);
   meshManager& meshes = gfx.getResourceManager<mesh>();
   materialManager& mats = gfx.getResourceManager<material>();
 
-  std::string mainScene = "main";
   shader shade("vertex.glsl", "fragment.glsl");
-  std::shared_ptr<scenenode> entRoot(scene.getRootNode().createChild());
+  std::shared_ptr<node> entRoot(scene.getRootNode().createChild());
   camera cam;
 
   gfx.setMainScene(mainScene);
@@ -32,10 +33,10 @@ int main(){
   scene.setMainCamera("main");
   meshes.acquire("cube", "data/cubePTN.flat");
   mats.acquire("box", "data/container.jpg");
-  entRoot.translate(0, 3, 0);
+  entRoot->translate(0, 3, 0);
 
   auto ent = std::make_shared<entity>(meshes.getResource("cube"), mats.getResource("box"));
-  entRoot.attach(ent, shade);
+  entRoot->attach(ent, shade);
 
   while(gfx.getRunning()){
     gfx.renderMainScene();
